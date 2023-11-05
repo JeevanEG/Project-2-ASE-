3e58e59591dd80c662aba5151b42b3f19c304123
jeevaneg <jeevaneg2004@gmail.com>
2023-11-05 13:09:57 +0530

    Newly pushed file

---
+++ trail.py
@@ -0,0 +1,14 @@
+"""
+This is a simple Python program that calculates the sum of two numbers.
+"""
+
+# Define two variables and give them meaningful names.
+# Use uppercase letters for constants as per naming conventions.
+A = 1
+B = 1
+
+# Calculate the sum of A and B.
+ADD = A + B
+
+# Print the result.
+print(ADD)
